c2307c7f515ba8df72702407f265dd8494f55234
Alex Haskins <alexanderwantshislibraryback@gmail.com>
2024-02-10 15:06:07 -0500

    think I finally succeded in installdots. If there are no more updates - success.

--- .tools/installdots.sh
+++ .tools/installdots.sh
@@ -4,15 +4,13 @@ source_dir="$HOME/dotfiles" # Define the source directory
 destination_dir="$HOME" # Define the destination directory
 
 shopt -s dotglob
-for file in "$source_dir/*"; do # Iterate over each file in $HOME/dotfiles/
+for file in "$source_dir"/*; do # Iterate over each file in $HOME/dotfiles/
     relative_path=${file#$source_dir/} # Get the relative path (to $HOME/dotfiles) of the file
-    if [ -e "$destination_dir/$relative_path" ]; then # If it exists,
-        if [ -d "$destination_dir/$relative_path" ]; then # If it's a directory,
-            rm -rf "$destination_dir/$relative_path" # (directory) kill it
-        else 
-            rm -f "$destination_dir/$relative_path" # (file) kill that too
+    if [ -d "$destination_dir/$relative_path" ]; then
+            rm -rf "$destination_dir/$relative_path"
+        else
+            rm -f "$destination_dir/$relative_path"
         fi
-    fi
 done
 
 cd "$source_dir" # heads to the dots directory
